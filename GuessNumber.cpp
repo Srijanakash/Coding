60f71f82afb580034476c525ce474c1bcd9b03b3
Srijan Nag <33724590+Srijanakash@users.noreply.github.com>
2020-04-25 19:57:16 +0530

    Create GuessNumber.cpp

---
+++ GuessNumber.cpp
@@ -0,0 +1,43 @@
+/*
+We are playing the Guess Game. The game is as follows:
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example :
+
+Input: n = 10, pick = 6
+Output: 6
+*/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution 
+{
+public:
+    int guessNumber(int n) 
+    {
+        int l=0,r=n,mid;
+        while(l<=r)
+        {
+            mid=l+(r-l)/2;
+            if(guess(mid)==0)
+                break;
+            if(guess(mid)>0)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        return mid;
+    }
+};

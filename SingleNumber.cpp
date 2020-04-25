56123f806674ebee7753f815e0ad368664ab6987
Srijan Nag <33724590+Srijanakash@users.noreply.github.com>
2020-04-25 21:26:39 +0530

    Create SingleNumber.cpp

---
+++ SingleNumber.cpp
@@ -0,0 +1,29 @@
+/*
+Given a non-empty array of integers, every element appears twice except for one. 
+Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+*/
+
+class Solution 
+{
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int x=nums[0],n=nums.size();
+        for(int i=1;i<n;i++)
+            x=x^nums[i];
+        return x;
+    }
+};

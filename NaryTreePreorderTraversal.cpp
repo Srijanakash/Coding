0a88b15a3a0b183c7de615f1a1c597c485be1f43
Srijan Nag <33724590+Srijanakash@users.noreply.github.com>
2020-05-03 18:51:12 +0530

    Create NaryTreePreorderTraversal.cpp

---
+++ NaryTreePreorderTraversal.cpp
@@ -0,0 +1,62 @@
+/*
+Given an n-ary tree, return the preorder traversal of its nodes' values.
+Nary-Tree input serialization is represented in their level order traversal, 
+each group of children is separated by the null value (See examples).
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,3,5,6,2,4]
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+*/
+
+/*
+// Definition for a Node.
+class Node 
+{
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) 
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) 
+    {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution 
+{
+public:
+    vector<int> preorder(Node* root) 
+    {
+        vector<int>res;
+        if(!root)
+            return res;
+        stack<Node *>s;
+        s.push(root);
+        while(!s.empty())
+        {
+            Node *temp=s.top();
+            s.pop();
+            res.push_back(temp->val);
+            for(auto i=temp->children.rbegin();i!=temp->children.rend();i++)
+                s.push(*i);
+        }
+        return res;
+    }
+};

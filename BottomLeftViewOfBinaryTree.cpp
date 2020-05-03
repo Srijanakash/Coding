b9f7ba11fd632cc2469056e35661be083ff6dfa7
Srijan Nag <33724590+Srijanakash@users.noreply.github.com>
2020-05-03 20:01:02 +0530

    Create BottomLeftViewOfBinaryTree.cpp

---
+++ BottomLeftViewOfBinaryTree.cpp
@@ -0,0 +1,65 @@
+/*
+Given a binary tree, find the leftmost value in the last row of the tree.
+
+Example 1:
+Input:
+
+    2
+   / \
+  1   3
+
+Output:
+1
+Example 2:
+Input:
+
+        1
+       / \
+      2   3
+     /   / \
+    4   5   6
+       /
+      7
+
+Output:
+7
+Note: You may assume the tree (i.e., the given root node) is not NULL.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    int findBottomLeftValue(TreeNode* root) 
+    {
+        int reqVal=0;
+        queue<TreeNode *>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int nodeCount=q.size();
+            for(int i=0;i<nodeCount;i++)
+            {
+                TreeNode *temp=q.front();
+                q.pop();
+                if(i==0)
+                    reqVal=temp->val;
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+        }
+        return reqVal;
+    }
+};

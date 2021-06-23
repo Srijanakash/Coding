79edcc0117603bec33049b1a927eedfb24972354
Srijanakash <srijannag123@gmail.com>
2021-06-23 22:32:08 +0530

    New Codes

---
+++ LeetCode/Practice/SortAnArray.cpp
@@ -0,0 +1,26 @@
+/*
+ * Given an array of integers nums, sort the array in ascending order.
+
+
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+ */
+
+
+
+class Solution
+{
+public:
+    vector<int> sortArray(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
